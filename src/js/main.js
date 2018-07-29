d5c1c41269c6afd6874d7ff7b02dde7014529004
MARS\Joohyoung <silentsky@hotmail.com>
2018-07-30 03:55:19 +0900

    - remove dist folder - move project data to seperated json file. (js/projs.js)

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,3 @@
-// Project data.
-const projs = '{ "projects" : [' +
-  '{ "id": 1, "name": "Modern Profile", "image": "img/projects/modern_profile.png", "site": "https://muhayou.github.io/modern_portfolio/", "repos": { "url": "https://github.com/muhayou/modern_portfolio", "type": "github" } },' +
-  '{ "id": 2, "name": "Organic Shop", "image": "img/projects/project1.jpg", "site": "#", "repos": { "url": "#", "type": "gitlab" } },' +
-  '{ "id": 3, "name": "RAMS", "image": "img/projects/project2.jpg", "site": "#", "repos": { "url": "#", "type": "gitlab" } },' +
-  '{ "id": 4, "name": "DPS", "image": "img/projects/project3.jpg", "site": "#", "repos": { "url": "#", "type": "gitlab" } },' +
-  '{ "id": 5, "name": "재난영상 연계", "image": "img/projects/project4.jpg", "site": "#", "repos": { "url": "#", "type": "gitlab" } },' +
-  '{ "id": 6, "name": "경기재난안전본부 유지보수", "image": "img/projects/gg_fire.png", "site": "#", "repos": { "url": "#", "type": "gitlab" } }' +
-']}';
-
 // Select DOM items.
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
@@ -65,6 +55,8 @@ function generateProject() {
   
   let tag = document.getElementsByClassName("lg-heading");
   tag[0].innerText = proj.name;
+
+  
 }
 
 /**
