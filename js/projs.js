9e7bc3763de9496e1df70363f9a3d76d3c777b9b
MARS\Joohyoung <silentsky@hotmail.com>
2018-07-30 03:56:35 +0900

    Updates

---
+++ js/projs.js
@@ -0,0 +1,9 @@
+// Project data.
+projs = '{ "projects" : [' +
+  '{ "id": 1, "name": "Modern Profile", "image": "img/projects/modern_profile.png", "site": "https://muhayou.github.io/modern_portfolio/", "repos": { "url": "https://github.com/muhayou/modern_portfolio", "type": "github" } },' +
+  '{ "id": 2, "name": "Organic Shop", "image": "img/projects/project1.jpg", "site": "#", "repos": { "url": "#", "type": "gitlab" } },' +
+  '{ "id": 3, "name": "RAMS", "image": "img/projects/project2.jpg", "site": "#", "repos": { "url": "#", "type": "gitlab" } },' +
+  '{ "id": 4, "name": "DPS", "image": "img/projects/project3.jpg", "site": "#", "repos": { "url": "#", "type": "gitlab" } },' +
+  '{ "id": 5, "name": "재난영상 연계", "image": "img/projects/project4.jpg", "site": "#", "repos": { "url": "#", "type": "gitlab" } },' +
+  '{ "id": 6, "name": "경기재난안전본부 유지보수", "image": "img/projects/gg_fire.png", "site": "#", "repos": { "url": "#", "type": "gitlab" } }' +
+']}';
